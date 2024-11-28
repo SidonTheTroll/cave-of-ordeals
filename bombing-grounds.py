11ed99b481e45d6681f1e0edfad3b793f7306353
Sidon_OO0 <70560640+SidonTheTroll@users.noreply.github.com>
2024-11-28 07:37:58 +0530

    Added launch.json and cleared testing file

--- bombing-grounds.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-# import numpy as np 
-
-a = [[7,6,6,8],[8,8,5,6],[7,6,6,9],[7,6,4,6]]
-
-df = pd.DataFrame(a, index=("Andhra",'Odisha','MP','UP'), columns=('Toys','Books','Uniform','Shoes'))
-
-print(df.loc['Andhra':'MP',:])
